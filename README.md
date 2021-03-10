f15563f8bac254fa7f83f439f316f611ff101313
Dave Smart <d.smart@smartinternetconsultants.co.uk>
2021-03-10 18:14:15 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TwitterScreenShot
 A Node CLI tool to make a screenshot of a tweet and create a link
+
+## Install
+Download the files from git (clone or download the zip)
+run `npm install`
+amend config.js to suit
+
+## Usage
+`node gettweet url={tweet_url here}`
---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+    imgDir: "tweetimg/", //where the tweet snapshot is saved
+    imgURL: "http://example.com/tweetimg/", // the URL directory (used in the <img> screen),
+    classNames: "" // add classes to the outputted html
+}
+
+module.exports = { config }
